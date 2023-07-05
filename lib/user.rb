26e216c21f61818a97733bab6f945e15c61ae5a2
Alex <alexkimanthi92@gmail.com>
2023-07-05 22:53:35 +0300

    updated

---
+++ lib/user.rb
@@ -0,0 +1,25 @@
+
+class User
+
+    attr_accessor :first_name, :last_name
+    def initialize(first_name, last_name)
+        @first_name = first_name
+        @last_name = last_name
+    end
+
+    # def first_name=(new_first_name)
+    #     @first_name = new_first_name 
+    # end
+
+    # def first_name
+    #     @first_name
+    # end
+
+    # def last_name
+    #     @last_name
+    # end
+
+    # def last_name=(new_last_name)
+    #     @last_name = new_last_name
+    # end
+end
